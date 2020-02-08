09add5e07fa7dde9ef29ab648330f521b87df48c
Gustavo Fraga <gustavohenriquemiele@live.com>
2020-02-08 11:31:18 -0300

    adicionando useDelete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import useGet from './useGet'
 import usePost from './usePost'
+import useDelete from './useDelete'
 import axios from 'axios'
 
 const url = 'https://mymoney-gus.firebaseio.com/movimentacoes/2020-02.json'
@@ -9,9 +10,13 @@ const url = 'https://mymoney-gus.firebaseio.com/movimentacoes/2020-02.json'
 function App() {
   const data = useGet(url)
   const [postData, post] = usePost(url)
-
+  const [deleteData, remove] = useDelete()
+ 
   const saveNew = () => {
     post({valor: 10, descricao: 'ola'})
+  }
+  const doRemove = () => {
+    remove('https://mymoney-gus.firebaseio.com/movimentacoes/2020-02/-M-_27mJRP5WnXioO6w4.json')
   }
 
   return (
@@ -21,6 +26,8 @@ function App() {
       { data.loading &&  <p>Loading...</p> }
       <button onClick={saveNew}>Salvar</button>
       <pre>{JSON.stringify(postData)}</pre>
+      <button onClick={doRemove}>Delete</button>
+      <pre>{JSON.stringify(deleteData)}</pre>
     </div>
   )
 }
